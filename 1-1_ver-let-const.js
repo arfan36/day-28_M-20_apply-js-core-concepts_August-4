e9a51b02c74cd66bf17308e8ec243b80c95e4a64
arfan36 <arfanur36@gmail.com>
2022-08-06 11:09:30 +0600

    20-1 JS Concepts recap...

---
+++ 1-1_ver-let-const.js
@@ -0,0 +1,7 @@
+let age = 11;
+// let age = 13;
+age = 12;
+
+const countryName = 'Bangladesh';
+// countryName = 'Chad';
+console.log(age, countryName);
---
+++ 1_recap.js
@@ -0,0 +1,47 @@
+// 1. Variable
+var myName = 'Khan';
+
+// math operation: +, -, *, /
+// shorthand: +=, --, *=, /=
+// ++, --
+
+// 2. array
+var friends = ['abul', 'babul', 'cabul', 'cabul', 'dabul', 'ebul'];
+
+var thirdFriend = friends[2];
+friends[3] = 'dambul';
+
+// 3. conditionals
+// >, <, >=, <=, ==, ===, !=, !==
+
+if (friends.length < 2) {
+    console.log('fokir tor kono friend nai');
+}
+else {
+
+}
+
+// 4. loop
+var number = 0;
+while (number <= 6) {
+    console.log(number);
+    number++;
+}
+
+for (var i = 0; i <= 10; i++) {
+
+}
+
+// 5. function
+function isMoonUp(time) {
+    if (time > 7) {
+        return true;
+    }
+}
+
+// 6. Object
+var jantus = {
+    height: 60,
+    romenticism: 'heavvy',
+    bappErTaka: 'borolox'
+};
