5a6c286a9a0f362a0ed4d4599a11efc37fe9cad7
arfan36 <arfanur36@gmail.com>
2022-08-07 15:28:12 +0600

    20-6 Get Odd Numbers

--- 5_findOddSum.js
+++ 5_findOddSum.js
@@ -5,9 +5,27 @@ function getSumOfAnArray(numbers) {
         const element = numbers[index];
         // sum = sum + element;
         sum += element;
-        console.log(index, element, sum);
+        // console.log(index, element, sum);
     }
     return sum;
 }
+
+function getOddNumbersOfAnArray(numbers) {
+    const oddNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        if (element % 2 !== 0) {
+            console.log(index, element);
+            oddNumbers.push(element);
+        }
+    }
+    return oddNumbers;
+}
+
 const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-getSumOfAnArray(myNumbers);
+const oddNumbers = getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
+const oddNumbersSum = getSumOfAnArray(oddNumbers);
+
+console.log('odd number sum', oddNumbersSum);
